ad23ecf2a388c7ac50619c314fd37b4d8e26e8ae
jbtrystram <blitzkrieg263@gmail.com>
2016-11-02 13:39:52 +0100

    clean python code

--- container_pika/sense.py
+++ container_pika/sense.py
@@ -35,36 +35,6 @@ hostname = open('/etc/hostname').read()
 sense = SenseHat()
 
 
-#AMQP settings and connection
-class Sender(MessagingHandler):
-    def __init__(self, server, address, message_body):
-        super(Sender, self).__init__()
-
-        self.server = server
-        self.address = address
-        self.message_body = message_body
-
-        self.sent = False
-
-    def on_start(self, event):
-        conn = event.container.connect(self.server)
-        event.container.create_sender(self.address)
-        print("SENDER: Created sender for target address '{0}'".format(self.address))
-
-    def on_sendable(self, event):
-        if self.sent:
-            return
-
-        message = Message(self.message_body)
-        event.sender.send(message)
-
-        print("SENDER: Sent message '{0}'".format(self.message_body))
-
-        event.connection.close()
-        self.sent = True
-
-
-
 def sendMessage(temp):
         amqpMsgPayload = {}
         amqpMsgPayload["timestamp"] = int((time.time()*1000))
