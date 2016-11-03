df6026197243cbf71d88dc8f8f7a3b874f0fe343
jbtrystram <blitzkrieg263@gmail.com>
2016-11-03 09:38:21 +0100

    add a bare instance to improve high availability

--- .start.sh
+++ .start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+export JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt
+rm /usr/bin/java && rm /usr/bin/javac 
+ln -s /usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/bin/java /usr/bin/java 
+ln -s /usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/bin/javac /usr/bin/javac
+
 cd /demo/
-java -jar fatjars/demo-dynamid-Receiver.java -cluster -cluster-host 10.45.0.51 
+sleep 15
+java -jar /demo/fatjars/demo-dynamid-Receiver-fat.jar -cluster -cluster-host 10.45.0.52
